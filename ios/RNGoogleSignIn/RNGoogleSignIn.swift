7596f4e7f31a6b200febae5359db89537da6e5be
Alex Thomas <cawfree@Alexs-Mac-mini.local>
2018-10-21 20:23:04 +0100

    Enabled support for multiple registered emitter to trigger events, instead of using a single events source that is re-written. This approach is not thread safe and is inflexible. But it works, even with cocoapods.

--- ios/RNGoogleSignIn/RNGoogleSignIn.swift
+++ ios/RNGoogleSignIn/RNGoogleSignIn.swift
@@ -8,15 +8,96 @@
 import Foundation
 
 @objc(RNGoogleSignIn)
-class RNGoogleSignIn: NSObject, GIDSignInUIDelegate {
+class RNGoogleSignIn: NSObject, GIDSignInUIDelegate, GIDSignInDelegate {
+  
+  static func userToJSON(_ user: GIDGoogleUser?) -> [String: Any]? {
+    if let user = user {
+      var body: [String: Any] = [:]
+      
+      if let userID = user.userID {
+        body["userID"] = userID
+      }
+      
+      if let profile = user.profile {
+        if let email = profile.email {
+          body["email"] = email
+        }
+        if let name = profile.name {
+          body["name"] = name
+        }
+        if let givenName = profile.givenName {
+          body["givenName"] = givenName
+        }
+        if let familyName = profile.familyName {
+          body["familyName"] = familyName
+        }
+        if profile.hasImage {
+          if let url = profile.imageURL(withDimension: 320)?.absoluteString {
+            body["imageURL320"] = url
+          }
+          if let url = profile.imageURL(withDimension: 640)?.absoluteString {
+            body["imageURL640"] = url
+          }
+          if let url = profile.imageURL(withDimension: 1280)?.absoluteString {
+            body["imageURL1280"] = url
+          }
+        }
+      }
+      
+      if let authentication = user.authentication {
+        if let clientID = authentication.clientID {
+          body["clientID"] = clientID
+        }
+        if let accessToken = authentication.accessToken {
+          body["accessToken"] = accessToken
+        }
+        if let accessTokenExpirationDate = authentication.accessTokenExpirationDate {
+          body["accessTokenExpirationDate"] = accessTokenExpirationDate.timeIntervalSince1970
+        }
+        if let refreshToken = authentication.refreshToken {
+          body["refreshToken"] = refreshToken
+        }
+        if let idToken = authentication.idToken {
+          body["idToken"] = idToken
+        }
+        if let idTokenExpirationDate = authentication.idTokenExpirationDate {
+          body["idTokenExpirationDate"] = idTokenExpirationDate.timeIntervalSince1970
+        }
+      }
+      
+      if let accessibleScopes = user.accessibleScopes {
+        body["accessibleScopes"] = accessibleScopes
+      }
+      
+      if let grantedScopes = user.grantedScopes {
+        body["grantedScopes"] = grantedScopes
+      }
+      
+      if let hostedDomain = user.hostedDomain {
+        body["hostedDomain"] = hostedDomain
+      }
+      
+      if let serverAuthCode = user.serverAuthCode {
+        body["serverAuthCode"] = serverAuthCode
+      }
+      
+      return body
+    } else {
+      return nil
+    }
+  }
 
   static let sharedInstance = RNGoogleSignIn()
+  
+  // TODO: How to make thread safe?
+  static var emitters = [RNGoogleSignInEvents]()
 
-  weak var events: RNGoogleSignInEvents?
+  //weak var events: RNGoogleSignInEvents?
 
   override init() {
     super.init()
     GIDSignIn.sharedInstance().uiDelegate = self
+    GIDSignIn.sharedInstance().delegate   = self
   }
 
   //  @objc func addEvent(_ name: String, location: String, date: NSNumber, callback: @escaping (Array<String>) -> ()) -> Void {
@@ -97,7 +178,7 @@ class RNGoogleSignIn: NSObject, GIDSignInUIDelegate {
   }
   
   @objc func currentUser(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    resolve(RNGoogleSignInEvents.userToJSON(GIDSignIn.sharedInstance().currentUser))
+    resolve(RNGoogleSignIn.userToJSON(GIDSignIn.sharedInstance().currentUser))
   }
   
   @objc func hasAuthInKeychain(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
@@ -125,7 +206,8 @@ class RNGoogleSignIn: NSObject, GIDSignInUIDelegate {
   // START: GIDSignInUIDelegate
   
   func sign(inWillDispatch signIn: GIDSignIn!, error: Error?) {
-    events?.dispatch(error: error)
+    // TODO: Refactor?
+    self.dispatch(error: error)
   }
 	
   func sign(_ signIn: GIDSignIn!, dismiss viewController: UIViewController!) {
@@ -164,5 +246,71 @@ class RNGoogleSignIn: NSObject, GIDSignInUIDelegate {
   }
 
   // END: GIDSignInUIDelegate
+  
+  // BEGIN: GIDSignInDelegate
+  
+  func signIn(user: GIDGoogleUser?) {
+    for emitter in RNGoogleSignIn.emitters {
+      if (emitter.isObserving()) {
+        emitter.sendEvent(withName: "signIn", body: RNGoogleSignIn.userToJSON(user))
+      }
+    }
+  }
+  
+  func signInError(error: Error?) {
+    for emitter in RNGoogleSignIn.emitters {
+      if (emitter.isObserving()) {
+        emitter.sendEvent(withName: "signInError", body: [
+          "description": error?.localizedDescription ?? "",
+        ])
+      }
+    }
+  }
+  
+  func disconnect(user: GIDGoogleUser?) {
+    for emitter in RNGoogleSignIn.emitters {
+      if (emitter.isObserving()) {
+        emitter.sendEvent(withName: "disconnect", body: RNGoogleSignIn.userToJSON(user))
+      }
+    }
+  }
+  
+  func disconnectError(error: Error?) {
+    for emitter in RNGoogleSignIn.emitters {
+      if (emitter.isObserving()) {
+        emitter.sendEvent(withName: "disconnectError", body: [
+          "description": error?.localizedDescription ?? "",
+          ])
+      }
+    }
+  }
+  
+  func dispatch(error: Error?) {
+    for emitter in RNGoogleSignIn.emitters {
+      if (emitter.isObserving()) {
+        emitter.sendEvent(withName: "dispatch", body: [
+          "description": error?.localizedDescription ?? "",
+        ])
+      }
+    }
+  }
+  
+  func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser?, withError error: Error?) {
+    if (error == nil && user != nil) {
+      self.signIn(user: user)
+    } else {
+      self.signInError(error: error)
+    }
+  }
+  
+  func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser?, withError error: Error?) {
+    if (error == nil) {
+      self.disconnect(user: user)
+    } else {
+      self.disconnectError(error: error)
+    }
+  }
+  
+  // END: GIDSignInDelegate
 
 }
--- ios/RNGoogleSignIn/RNGoogleSignInEventsBridge.m
+++ ios/RNGoogleSignIn/RNGoogleSignInEventsBridge.m
@@ -15,6 +15,14 @@
   #import "React/RCTBridgeModule.h"
 #endif
 
-@interface RCT_EXTERN_MODULE(RNGoogleSignInEvents, NSObject)
+#if __has_include(<React/RCTEventEmitter.h>)
+  #import <React/RCTEventEmitter.h>
+#elif __has_include("RCTEventEmitter.h")
+  #import "RCTEventEmitter.h"
+#else
+  #import "React/RCTEventEmitter.h"
+#endif
+
+@interface RCT_EXTERN_MODULE(RNGoogleSignInEvents, RCTEventEmitter)
 
 @end
